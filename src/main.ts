ab847baa4b5d380bb13cc8b362a4ffae1fb2029e
Toniton <akinjiolatoni@gmail.com>
2020-01-21 21:53:30 +0100

    Setup deploy strategy for production

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  // const app = await NestFactory.createMicroservice(AppModule, {
-  //   transport: Transport.TCP,
-  //   options: {
-  //     port: 3001,
-  //   },
-  // });
-  // app.listen(() => console.log('Microservice is listening'));
   const app = await NestFactory.create(AppModule);
-  const microservice = app.connectMicroservice({
-    transport: Transport.TCP,
-    options: {
-      port: 3001,
-    },
-  });
-
   await app.startAllMicroservicesAsync();
-  await app.listen(3002);
+  await app.listen(8090);
 }
 bootstrap();
